ee18defeafbc351b24ba24d08f800ee4a6498325
Oleh <mandra9012003@gmail.com>
2022-06-20 21:19:11 +0300

    (Interfaces) Created interface "IUserRepository" inherited "IRepository"

---
+++ DAL/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace DAL.Interfaces
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<IEnumerable<User>> GetAllWithDetailsAsync();
+
+        Task<User> GetByIdWithDetailsAsync(int id);
+    }
+}
