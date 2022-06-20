ebb4ec83fe5fc78f1f18788ad18380bff7514956
Oleh <mandra9012003@gmail.com>
2022-06-20 21:49:16 +0300

    (Data) Implemented pattern "UnitOfWork"

---
+++ DAL/Data/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using DAL.Interfaces;
+using DAL.Repositories;
+using System.Threading.Tasks;
+
+namespace DAL.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private AuctionDbContext DbContext { get; set; }
+
+        public UnitOfWork(AuctionDbContext dbContext)
+        {
+            this.DbContext = dbContext;
+        }
+
+        public UnitOfWork()
+        {
+            this.DbContext = new AuctionDbContext();
+        }
+
+        public IUserRepository UserRepository => new UserRepository(this.DbContext);
+
+        public ILotRepository LotRepository => new LotRepository(this.DbContext);
+
+        public IPhotoRepository PhotoRepository => new PhotoRepository(this.DbContext);
+
+        public IOrderRepository OrderRepository => new OrderRepository(this.DbContext);
+
+        public IOrderDetailRepository OrderDetailRepository => new OrderDetailRepository(this.DbContext);
+
+        public async Task SaveAsync()
+                    => await this.DbContext.SaveChangesAsync();
+    }
+}
