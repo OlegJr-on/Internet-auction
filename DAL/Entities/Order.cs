0d7f8ff8c9c3ca0c00e8712a7a3ce79217863c61
Oleh <mandra9012003@gmail.com>
2022-06-20 21:03:53 +0300

    (Entities) Added user entity

---
+++ DAL/Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.Entities
+{
+    public class Order : BaseEntity
+    {
+        public int UserId { get; set; }
+        public DateTime OperationDate { get; set; }
+        public User User { get; set; }
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
