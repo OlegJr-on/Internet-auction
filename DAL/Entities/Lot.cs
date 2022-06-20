0d8a0899a75f0085b7bff4a6fd6974c084424d7a
Oleh <mandra9012003@gmail.com>
2022-06-20 21:03:10 +0300

    (Entities) Added lot entity

---
+++ DAL/Entities/Lot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.Entities
+{
+    public class Lot : BaseEntity
+    {
+        public string Title { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public decimal StartPrice { get; set; }
+        public int MinRate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public LotStatus Status { get; set; }
+        public int PhotoId { get; set; }
+        public Photo Photo { get; set; }
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
