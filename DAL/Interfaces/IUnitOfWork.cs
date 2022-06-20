64b0efb35da81dd22f68866a72aaac70609c6e7b
Oleh <mandra9012003@gmail.com>
2022-06-20 21:21:59 +0300

    (Interfaces) Created interface "IUnitOfWork" which will implement the pattern Unit Of Work

---
+++ DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository UserRepository { get; }
+
+        ILotRepository LotRepository { get; }
+
+        IPhotoRepository PhotoRepository { get; }
+
+        IOrderRepository OrderRepository { get; }
+
+        IOrderDetailRepository OrderDetailRepository { get; }
+
+        Task SaveAsync();
+    }
+}
