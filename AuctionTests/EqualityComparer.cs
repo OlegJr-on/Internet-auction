ca497afa8c1a6248c4efcc5720e40aa05818a504
Oleh <mandra9012003@gmail.com>
2022-06-20 22:16:08 +0300

    Added functionality to equality comparer entities in tests

---
+++ AuctionTests/EqualityComparer.cs
@@ -0,0 +1,90 @@
+﻿using DAL.Entities;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AuctionTests
+{
+    internal class UserEqualityComparer : IEqualityComparer<User>
+    {
+        public bool Equals([AllowNull] User x, [AllowNull] User y)
+        {
+            if (x == null && y == null)
+                return true;
+            if (x == null || y == null)
+                return false;
+
+            return x.Id == y.Id
+                && x.Password == y.Password;
+        }
+
+        public int GetHashCode([DisallowNull] User obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
+    internal class OrderEqualityComparer : IEqualityComparer<Order>
+    {
+        public bool Equals([AllowNull] Order x, [AllowNull] Order y)
+        {
+            if (x == null && y == null)
+                return true;
+            if (x == null || y == null)
+                return false;
+
+            return x.Id == y.Id
+                && x.UserId == y.UserId
+                && x.OperationDate == y.OperationDate;
+        }
+
+        public int GetHashCode([DisallowNull] Order obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
+    internal class LotEqualityComparer : IEqualityComparer<Lot>
+    {
+        public bool Equals([AllowNull] Lot x, [AllowNull] Lot y)
+        {
+            if (x == null && y == null)
+                return true;
+            if (x == null || y == null)
+                return false;
+
+            return x.Id == y.Id
+                && x.PhotoId == y.PhotoId
+                && x.Title == y.Title
+                && x.StartPrice == y.StartPrice
+                && x.StartDate == y.StartDate
+                && x.EndDate == y.EndDate;
+        }
+
+        public int GetHashCode([DisallowNull] Lot obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
+    internal class OrderDetailEqualityComparer : IEqualityComparer<OrderDetail>
+    {
+        public bool Equals([AllowNull] OrderDetail x, [AllowNull] OrderDetail y)
+        {
+            if (x == null && y == null)
+                return true;
+            if (x == null || y == null)
+                return false;
+
+            return x.Id == y.Id
+                && x.OrderId == y.OrderId
+                && x.LotId == y.LotId
+                && x.Status == y.Status;
+        }
+
+        public int GetHashCode([DisallowNull] OrderDetail obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
+}
