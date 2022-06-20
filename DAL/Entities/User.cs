0ecf8bcf7a44a75335b0c72414edb6a7e3f31f21
Oleh <mandra9012003@gmail.com>
2022-06-20 21:08:47 +0300

    (Entities) Added user entity

---
+++ DAL/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace DAL.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Location { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICollection<Order> Orders { get; set; }
+        public Role AccessLevel { get; set; }
+    }
+}
