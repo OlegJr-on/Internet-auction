1142f1c5a80643a7e6faa90a0ffb05b173a341f8
Oleh <mandra9012003@gmail.com>
2022-06-20 21:39:23 +0300

    (Data) Created database class which will connected to local database "AuctionDb"

---
+++ DAL/Data/AuctionDbContext.cs
@@ -0,0 +1,82 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Data
+{
+    public class AuctionDbContext : DbContext
+    {
+        public AuctionDbContext()
+        { }
+
+        public AuctionDbContext(DbContextOptions<AuctionDbContext> options) : base(options)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=AuctionDb;Trusted_Connection=True;");
+        }
+
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Lot> Lots { get; set; }
+
+        public DbSet<Photo> Photos { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new LotConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new PhotoConfiguration());
+        }
+    }
+
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(40);
+            builder.Property(p => p.Surname).IsRequired().HasMaxLength(60);
+            builder.Property(p => p.PhoneNumber).IsRequired().HasMaxLength(15);
+            builder.Property(p => p.Email).IsRequired().HasMaxLength(60);
+            builder.Property(p => p.Password).IsRequired().HasMaxLength(50);
+
+            builder.HasMany(c => c.Orders).WithOne(t => t.User);
+        }
+    }
+
+    public class LotConfiguration : IEntityTypeConfiguration<Lot>
+    {
+        public void Configure(EntityTypeBuilder<Lot> builder)
+        {
+            builder.Property(p => p.Title).IsRequired().HasMaxLength(150);
+            builder.Property(p => p.StartDate).HasColumnType("DateTime").IsRequired();
+            builder.Property(p => p.EndDate).HasColumnType("DateTime").IsRequired();
+
+            builder.HasMany(p => p.OrderDetails).WithOne(p => p.Lot);
+        }
+    }
+
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasMany(r => r.OrderDetails).WithOne(r => r.Order);
+        }
+    }
+
+    public class PhotoConfiguration : IEntityTypeConfiguration<Photo>
+    {
+        public void Configure(EntityTypeBuilder<Photo> builder)
+        {
+            builder.Property(et => et.GroupOfPhoto).IsRequired();
+        }
+    }
+
+}
---
+++ DAL/Migrations/20220620183732_Initial.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PhotoSrc = table.Column<string>(nullable: true),
+                    GroupOfPhoto = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 40, nullable: false),
+                    Surname = table.Column<string>(maxLength: 60, nullable: false),
+                    Location = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(maxLength: 15, nullable: false),
+                    Email = table.Column<string>(maxLength: 60, nullable: false),
+                    Password = table.Column<string>(maxLength: 50, nullable: false),
+                    AccessLevel = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lots",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(maxLength: 150, nullable: false),
+                    CurrentPrice = table.Column<decimal>(nullable: false),
+                    StartPrice = table.Column<decimal>(nullable: false),
+                    MinRate = table.Column<int>(nullable: false),
+                    StartDate = table.Column<DateTime>(type: "DateTime", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "DateTime", nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    PhotoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lots", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Lots_Photos_PhotoId",
+                        column: x => x.PhotoId,
+                        principalTable: "Photos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(nullable: false),
+                    OperationDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(nullable: false),
+                    LotId = table.Column<int>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Lots_LotId",
+                        column: x => x.LotId,
+                        principalTable: "Lots",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lots_PhotoId",
+                table: "Lots",
+                column: "PhotoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_LotId",
+                table: "OrderDetails",
+                column: "LotId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderId",
+                table: "OrderDetails",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "Lots");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Photos");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
