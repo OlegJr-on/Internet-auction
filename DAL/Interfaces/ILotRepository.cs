b596396c071239c2003a69b440b17b8811728ae6
Oleh <mandra9012003@gmail.com>
2022-06-20 21:15:53 +0300

    (Interfaces) Created interface "ILotRepository" inherited "IRepository"

---
+++ DAL/Interfaces/ILotRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace DAL.Interfaces
+{
+    public interface ILotRepository : IRepository<Lot>
+    {
+        Task<IEnumerable<Lot>> GetAllWithDetailsAsync();
+
+        Task<Lot> GetByIdWithDetailsAsync(int id);
+    }
+}
