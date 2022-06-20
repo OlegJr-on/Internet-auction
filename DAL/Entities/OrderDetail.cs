efb6f2bf83808aa2c2c09decdee413db30aec1d4
Oleh <mandra9012003@gmail.com>
2022-06-20 21:05:24 +0300

    (Entities) Added order detail entity

---
+++ DAL/Entities/OrderDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class OrderDetail : BaseEntity
+    {
+        public int OrderId { get; set; }
+        public int LotId { get; set; }
+        public LotDetailStatus Status { get; set; }
+        public Order Order { get; set; }
+        public Lot Lot { get; set; }
+    }
+}
