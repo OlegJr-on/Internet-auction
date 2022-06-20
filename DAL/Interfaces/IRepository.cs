986c07d0e300152b3062acc470806cb4f85ed293
Oleh <mandra9012003@gmail.com>
2022-06-20 21:13:55 +0300

    (Interfaces) Created basic interface "IRepository"

---
+++ DAL/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace DAL.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        Task<TEntity> GetByIdAsync(int id);
+
+        Task AddAsync(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        Task DeleteByIdAsync(int id);
+
+        void Update(TEntity entity);
+    }
+}
