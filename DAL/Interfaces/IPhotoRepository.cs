a4682da711bd26e695206c49035c0b2f2ca2770b
Oleh <mandra9012003@gmail.com>
2022-06-20 21:18:27 +0300

    (Interfaces) Created interface "IPhotoRepository" inherited "IRepository"

---
+++ DAL/Interfaces/IPhotoRepository.cs
@@ -0,0 +1,11 @@
+﻿using DAL.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IPhotoRepository : IRepository<Photo>
+    {
+        Task<IEnumerable<Photo>> GetByGroupNumber(int id);
+    }
+}
