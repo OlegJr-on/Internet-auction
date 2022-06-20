afa09bcb35fa91c8d9746d6864d97cc2b198daab
Oleh <mandra9012003@gmail.com>
2022-06-20 21:01:42 +0300

    (Entities) Added  enum status for lot

---
+++ DAL/Entities/StatusLot.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace DAL.Entities
+{
+    public enum LotStatus
+    {
+        Created = 1,
+        Sold_Out = 2,
+        Traded = 3,
+        Not_sold = 4
+    }
+
+    public enum LotDetailStatus
+    {
+        Bid_placed = 1,
+        Sold_On = 2,
+        Interrupted_bit = 3,
+        Trade_ended = 4
+    }
+}
