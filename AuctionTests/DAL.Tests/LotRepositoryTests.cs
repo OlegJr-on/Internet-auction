e72bb08bdb6ece8867945757296a6b4925568efa
Oleh <mandra9012003@gmail.com>
2022-06-21 22:43:31 +0300

    Created class "LotRepositoryTests" which is responsible for tests with the lot entity

---
+++ AuctionTests/DAL.Tests/LotRepositoryTests.cs
@@ -0,0 +1,44 @@
+﻿using DAL.Data;
+using DAL.Entities;
+using DAL.Repositories;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuctionTests.DAL.Tests
+{
+    [TestFixture]
+    public class LotRepositoryTests {
+
+
+
+        private static IEnumerable<Lot> ExpectedLots =>
+            new[]
+            {
+                new Lot {
+                    Id = 1,
+                    Title = "2016 BMW 1er 116i Advantage",
+                    Status = LotStatus.Created,
+                    MinRate = 50,
+                    StartDate = new DateTime(2022, 6, 20, 20, 0, 0),
+                    EndDate = new DateTime(2022, 6, 22, 20, 0, 0),
+                    StartPrice = 1600,
+                    PhotoId = 1,
+                    CurrentPrice = 2100.33M,
+                },
+                new Lot {
+                    Id=2,
+                    Title = "2007 Volkswagen Golf V Plus Tour",
+                    Status = LotStatus.Created,
+                    MinRate = 110,
+                    StartDate = new DateTime(2022, 6, 25, 20, 0, 0),
+                    EndDate = new DateTime(2022, 6, 28, 20, 0, 0),
+                    StartPrice = 3400,
+                    PhotoId = 3,
+                    CurrentPrice = 8600.33M
+                }
+            };
+    }
+}
--- DAL/Data/AuctionDbContext.cs
+++ DAL/Data/AuctionDbContext.cs
@@ -14,7 +14,7 @@ namespace DAL.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=AuctionDb;Trusted_Connection=True;");
+            //optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=AuctionDb;Trusted_Connection=True;");
         }
 
 
