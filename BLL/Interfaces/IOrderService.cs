7928eb513ba75492be8fca884fc28e404d76af4f
Oleh <mandra9012003@gmail.com>
2022-06-25 01:15:32 +0300

    (Interfaces) Added interface which responsible of "OrderService"

---
+++ BLL/Interfaces/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IOrderService : ICrud<OrderModel>
+    {
+        Task AddLotAsync(int lotId, int orderId);
+
+        Task RemoveLotAsync(int lotId, int orderId);
+
+        Task<IEnumerable<OrderDetailModel>> GetOrderDetailsAsync(int orderId);
+
+        Task<IEnumerable<OrderModel>> GetOrdersByPeriodAsync(DateTime startDate, DateTime endDate);
+    }
+}
