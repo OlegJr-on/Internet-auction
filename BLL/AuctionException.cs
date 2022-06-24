aeed1032beec367cba40ff4a278f3e1629666237
Oleh <mandra9012003@gmail.com>
2022-06-25 01:22:38 +0300

    Created own exception "AuctionException"

---
+++ BLL/AuctionException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace BLL
+{
+    public class AuctionException : Exception
+    {
+        public AuctionException()
+        { }
+        public AuctionException(string message) : base(message)
+        { }
+        public AuctionException(string message, Exception inner)
+        : base(message, inner) { }
+    }
+}
