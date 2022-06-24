b33a0233aab0174716d325d6dea8cc5fd2e169e2
Oleh <mandra9012003@gmail.com>
2022-06-25 01:11:13 +0300

    Added basic interface "ICrud" which responsible of other service interfaces

---
+++ BLL/Interfaces/ICrud.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface ICrud<TModel> where TModel : class
+    {
+        Task<IEnumerable<TModel>> GetAllAsync();
+
+        Task<TModel> GetByIdAsync(int id);
+
+        Task AddAsync(TModel model);
+
+        Task UpdateAsync(TModel model);
+
+        Task DeleteAsync(int modelId);
+    }
+}
