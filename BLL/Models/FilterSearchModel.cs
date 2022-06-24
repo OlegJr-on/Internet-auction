571ff85c65bdaf43407412d1091a5cffd3a3a8fe
Oleh <mandra9012003@gmail.com>
2022-06-25 01:21:25 +0300

    (Models) Added model "FilterSearchModel"

---
+++ BLL/Models/FilterSearchModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace BLL.Models
+{
+    public class FilterSearchModel
+    {
+        public DateTime? BeginningPeriod { get; set; }
+        public DateTime? EndPeriod { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+    }
+}
