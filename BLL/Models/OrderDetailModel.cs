d927a21fe1badca37e64dadb3ccfa0afe5ea34c4
Oleh <mandra9012003@gmail.com>
2022-06-25 01:20:36 +0300

    (Models) Added model "OrderDetailModel"

---
+++ BLL/Models/OrderDetailModel.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Entities;
+
+
+namespace BLL.Models
+{
+    public class OrderDetailModel
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int LotId { get; set; }
+        public LotDetailStatus Status { get; set; }
+    }
+}
