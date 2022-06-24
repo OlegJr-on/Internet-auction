f95e4b332763ac5fe9425c8ade264e72cd42b6f7
Oleh <mandra9012003@gmail.com>
2022-06-25 01:13:18 +0300

    (Interfaces) Added interface which responsible of "LotService"

---
+++ BLL/Interfaces/ILotService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BLL.Models;
+
+
+namespace BLL.Interfaces
+{
+    public interface ILotService : ICrud<LotModel>
+    {
+        Task<IEnumerable<LotModel>> GetByFilterAsync(FilterSearchModel filterSearch);
+        Task<IEnumerable<PhotoModel>> GetAllPhotosAsync();
+        Task<IEnumerable<PhotoModel>> GetPhotosGroupByIdAsync(int id);
+        Task AddPhotoAsync(PhotoModel photoModel);
+        Task UpdatePhotoAsync(PhotoModel photoModel);
+        Task RemovePhotoAsync(int photoId);
+    }
+}
