c116251ea4d1b5314ab092599bb6578226e210e7
Oleh <mandra9012003@gmail.com>
2022-06-25 01:19:52 +0300

    (Models) Added model "OrderModel"

---
+++ BLL/Models/OrderModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BLL.Models
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime OperationDate { get; set; }
+        public ICollection<int> OrderDetailsIds { get; set; }
+    }
+}
