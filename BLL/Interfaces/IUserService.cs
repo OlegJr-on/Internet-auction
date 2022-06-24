e82983b47fdd9f6aa483e9838f1f32d7e8f0489e
Oleh <mandra9012003@gmail.com>
2022-06-25 01:14:34 +0300

    (Interfaces) Added interface which responsible of "UserService"

---
+++ BLL/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BLL.Models;
+
+namespace BLL.Interfaces
+{
+    public interface IUserService : ICrud<UserModel>
+    {
+        Task<IEnumerable<UserModel>> GetUsersByLotIdAsync(int LotId);
+    }
+}
