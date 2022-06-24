7c37ede33fa4940c04040ce00e79bcee4166c4bd
Oleh <mandra9012003@gmail.com>
2022-06-25 01:17:31 +0300

    (Models) Added model "LotModel"

---
+++ BLL/Models/LotModel.cs
@@ -0,0 +1,20 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace BLL.Models
+{
+    public class LotModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public decimal StartPrice { get; set; }
+        public int MinRate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public LotStatus Status { get; set; }
+        public int PhotoId { get; set; }
+        public ICollection<int> OrderDetailsIds { get; set; }
+    }
+}
