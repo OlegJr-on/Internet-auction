d2f08933827d32df6c823b49d14c6ac211e68323
Oleh <mandra9012003@gmail.com>
2022-06-25 01:18:17 +0300

    (Models) Added model "UserModel"

---
+++ BLL/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DAL.Entities;
+using System.Collections.Generic;
+
+namespace BLL.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Location { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICollection<int> OrdersIds { get; set; }
+        public Role AccessLevel { get; set; }
+    }
+}
