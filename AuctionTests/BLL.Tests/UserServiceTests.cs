59250f9ac3173050b252c0e3ba7ddb2535506a25
Oleh <mandra9012003@gmail.com>
2022-06-27 22:58:39 +0300

    Created class "UserServiceTests" which is responsible for tests with the user service

---
+++ AuctionTests/BLL.Tests/UserServiceTests.cs
@@ -0,0 +1,230 @@
+﻿using BLL.Models;
+using DAL.Entities;
+using DAL.Interfaces;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.Services;
+using FluentAssertions;
+using BLL;
+
+namespace AuctionTests.BLL.Tests
+{
+    public class UserServiceTests
+    {
+
+
+
+
+        public List<UserModel> GetTestUserModels =>
+           new List<UserModel>()
+           {
+                 new UserModel {
+                    Id = 1,
+                    Name = "Oleh",
+                    Surname = "Mandra",
+                    Location = "Ukraine,Kyiv",
+                    Email = "mandra@gmail.com",
+                    Password = "123",
+                    AccessLevel = Role.Admin,
+                    PhoneNumber = "095-937-3031",
+                    OrdersIds = new List<int>(){ 1 }
+                    },
+                new UserModel {
+                    Id = 2,
+                    Name = "Nassim",
+                    Surname = "Vakulenko",
+                    Location = "Ukraine,Lviv",
+                    Email = "vaN@gmail.com",
+                    Password = "12345",
+                    AccessLevel = Role.RegUser,
+                    PhoneNumber = "095-937-3044",
+                    OrdersIds = new List<int>(){ 2 }
+                    },
+                new UserModel {
+                    Id = 3,
+                    Name = "Mykola",
+                    Surname = "Iaremchuk",
+                    Location = "Ukraine,Kharkiv",
+                    Email = "vaN@gmail.com",
+                    Password = "1234445",
+                    AccessLevel = Role.RegUser,
+                    PhoneNumber = "095-737-3044",
+                    OrdersIds = new List<int>(){ 3 }
+                    },
+                new UserModel {
+                    Id = 4,
+                    Name = "Andriy",
+                    Surname = "Orenko",
+                    Location = "Ukraine,Kharkiv",
+                    Email = "vaN@gmail.com",
+                    Password = "1234104445",
+                    AccessLevel = Role.RegUser,
+                    PhoneNumber = "095-747-3044",
+                    OrdersIds = new List<int>(){ 4 }
+                    }
+           };
+
+
+        public List<User> GetTestUserEntities =>
+            new List<User>()
+            {
+                 new User {
+                    Id = 1,
+                    Name = "Oleh",
+                    Surname = "Mandra",
+                    Location = "Ukraine,Kyiv",
+                    Email = "mandra@gmail.com",
+                    Password = "123",
+                    AccessLevel = Role.Admin,
+                    PhoneNumber = "095-937-3031",
+                    Orders = new List<Order> { GetTestOrdersEntitiesWithOrderDetails[0] }
+                    },
+                  new User {
+                    Id = 2,
+                    Name = "Nassim",
+                    Surname = "Vakulenko",
+                    Location = "Ukraine,Lviv",
+                    Email = "vaN@gmail.com",
+                    Password = "12345",
+                    AccessLevel = Role.RegUser,
+                    PhoneNumber = "095-937-3044",
+                    Orders = new List<Order> { GetTestOrdersEntitiesWithOrderDetails[1] }
+                    },
+                new User {
+                    Id = 3,
+                    Name = "Mykola",
+                    Surname = "Iaremchuk",
+                    Location = "Ukraine,Kharkiv",
+                    Email = "vaN@gmail.com",
+                    Password = "1234445",
+                    AccessLevel = Role.RegUser,
+                    PhoneNumber = "095-737-3044",
+                    Orders = new List<Order> { GetTestOrdersEntitiesWithOrderDetails[2] }
+                    },
+                new User {
+                    Id = 4,
+                    Name = "Andriy",
+                    Surname = "Orenko",
+                    Location = "Ukraine,Kharkiv",
+                    Email = "vaN@gmail.com",
+                    Password = "1234104445",
+                    AccessLevel = Role.RegUser,
+                    PhoneNumber = "095-747-3044",
+                    Orders = new List<Order> { GetTestOrdersEntitiesWithOrderDetails[3] }
+                    }
+
+            };
+
+        public List<Order> GetTestOrdersEntitiesWithOrderDetails =>
+             new List<Order>()
+             {
+                new Order
+                {
+                    Id = 1,
+                    OperationDate = new DateTime(2022,12,02),
+                    UserId = 1,
+                    OrderDetails = new List<OrderDetail>()
+                    {
+                         new OrderDetail
+                        {
+                             Id = 1,
+                            LotId = 1,
+                            OrderId = 1,
+                            Status = LotDetailStatus.Bid_placed
+                        },
+                         new OrderDetail
+                        {
+                            Id = 2,
+                            LotId = 2,
+                            OrderId = 1,
+                            Status = LotDetailStatus.Bid_placed
+                        },
+                         new OrderDetail
+                        {
+                            Id = 3,
+                            LotId = 3,
+                            OrderId = 1,
+                            Status = LotDetailStatus.Bid_placed
+                        },
+                    }
+                },
+
+                new Order
+                {
+                    Id=2,
+                    OperationDate = new DateTime(2022, 8, 10),
+                    UserId = 2,
+                    OrderDetails = new List<OrderDetail>()
+                    {
+                         new OrderDetail
+                        {
+                             Id = 4,
+                            LotId = 1,
+                            OrderId = 2,
+                            Status = LotDetailStatus.Bid_placed
+                        },
+                         new OrderDetail
+                        {
+                            Id = 5,
+                            LotId = 3,
+                            OrderId = 2,
+                            Status = LotDetailStatus.Bid_placed
+                        },
+                    }
+                },
+
+                new Order
+                {
+                    Id= 3,
+                    OperationDate = new DateTime(2022, 10, 10),
+                    UserId = 3,
+                    OrderDetails = new List<OrderDetail>()
+                    {
+                        new OrderDetail
+                        {
+                             Id = 6,
+                            LotId = 1,
+                            OrderId = 3,
+                            Status = LotDetailStatus.Bid_placed
+                        },
+                        new OrderDetail
+                        {
+                            Id = 7,
+                            LotId = 2,
+                            OrderId = 3,
+                            Status = LotDetailStatus.Bid_placed
+                        },
+                    }
+                },
+
+                new Order
+                {
+                    Id= 4,
+                    OperationDate = new DateTime(2022, 12, 10),
+                    UserId = 4,
+                    OrderDetails = new List<OrderDetail>()
+                    {
+                        new OrderDetail
+                        {
+                             Id = 8,
+                            LotId = 5,
+                            OrderId = 4,
+                            Status = LotDetailStatus.Bid_placed
+                        },
+                        new OrderDetail
+                        {
+                            Id = 9,
+                            LotId = 6,
+                            OrderId = 4,
+                            Status = LotDetailStatus.Bid_placed
+                        },
+                    }
+                },
+             };
+    }
+
+}
