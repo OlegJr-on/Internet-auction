0ce4c6f6d524feda5d385263de2c4e565a94d72f
Oleh Mandra <mandra9012003@gmail.com>
2022-07-10 14:14:08 +0300

    (Models) Added class "UserLogin" which will correspond to the request sent by the user

---
+++ Web API/Models/UserLogin.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_API.Models
+{
+    public class UserLogin
+    {
+        [Required]
+        public string EmailAddress { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
