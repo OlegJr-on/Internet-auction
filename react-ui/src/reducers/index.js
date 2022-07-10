42c981154c7a70ce1ac5605d1d3eead7184c54e9
Oleh Mandra <mandra9012003@gmail.com>
2022-07-10 23:30:45 +0300

    (reducers) added "userReducer" and "fileReducer"

---
+++ react-ui/src/reducers/index.js
@@ -0,0 +1,14 @@
+import {applyMiddleware, combineReducers} from "redux";
+import { legacy_createStore as createStore} from 'redux'
+import {composeWithDevTools } from 'redux-devtools-extension'
+import thunk from "redux-thunk";
+import userReducer from "./userReducer";
+import fileReducer from "./fileReducer";
+
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    files: fileReducer,
+})
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
