64ad5a5c2f223c5c406fc26cc6e7fa988f874d40
Oleh Mandra <mandra9012003@gmail.com>
2022-07-10 23:12:28 +0300

    (layout) Created "Main" layer

---
+++ react-ui/src/layout/Main.jsx
@@ -0,0 +1,42 @@
+import {Route,Routes} from 'react-router-dom'
+import {Auction} from "../pages/Auction";
+import {About} from "../pages/About";
+import { Home } from "../pages/Home";
+import {Registration} from "../pages/Registration";
+import { SignIn } from "../pages/SignIn";
+import {Lot} from '../pages/Lot';
+import { NotFound } from '../pages/NotFound';
+import React, {useEffect} from 'react';
+import {useDispatch,useSelector} from 'react-redux'
+
+function Main(){
+
+    const isAuth = useSelector(state => state.user.isAuth)
+    const dispatch = useDispatch()
+  
+    useEffect(() => {
+        dispatch(auth())
+    }, [])
+
+    return (
+            <main className="container content">
+                <Routes>
+                    <Route path='/Auction' element={<Auction />}/>
+                    <Route path='/Auction/:lotId' element={<Lot/>}/>
+                    <Route path='/About' element={<About/>}/>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/Home' element={<Home/>}/>
+                    {!isAuth &&
+                     <>
+                    <Route path='/Registration' element={<Registration/>}/>
+                    <Route path='/SignIn' element={<SignIn/>}/>                    
+                    </>
+                    }
+
+                    <Route path="*" element={ <NotFound/>} />
+                </Routes>
+            </main>
+    )
+}
+
+export {Main}
