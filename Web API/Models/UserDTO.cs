822f5a33ee20a95c7d1b2728a953cf0298d9126d
Oleh Mandra <mandra9012003@gmail.com>
2022-07-10 14:09:28 +0300

    (Models) Created folder "Models" and added class "UserDTO" which represents user model

---
+++ Web API/Models/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Web_API.Models
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Location { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICollection<int> OrdersIds { get; set; }
+        public string AccessLevel { get; set; }
+    }
+}
