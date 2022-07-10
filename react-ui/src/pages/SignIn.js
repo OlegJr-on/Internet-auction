7ed7d9076d8ae2ef72e79bf5b81a60d1ff47366b
Oleh Mandra <mandra9012003@gmail.com>
2022-07-10 23:41:13 +0300

    (pages) added "SignIn.js" page, which represent "Login" section in site

---
+++ react-ui/src/pages/SignIn.js
@@ -0,0 +1,37 @@
+import React,{useState} from 'react';
+import { login } from '../actions/user';
+import {useDispatch} from "react-redux";
+
+
+export function SignIn(){
+
+        const [email,setEmail] = useState('')
+        const [password,setPassword] = useState('')
+        const dispatch = useDispatch()
+        
+        return (
+            <div className="registration-cssave">
+                <form>
+                    <h3 className="text-center">Login</h3>
+
+                    <div className="form-group">
+                        <input value={email} onChange={(e) => setEmail(e.target.value) } 
+                                className="form-control item" type="text" name="email" maxlength="50" minlength="4" pattern="^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$" id="email" placeholder="Email" required/>
+                    </div>
+
+                    <div className="form-group">
+                        <input value={password}   onChange={(e) => setPassword   (e.target.value) } 
+                                className="form-control item" type="password" name="password" minlength="6" id="password" placeholder="Password" required/>
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary btn-block create-account"
+                                 type="submit" 
+                                 disabled={!(!!(email)) || !(!!(password))}
+                                 onClick={() => dispatch(login(email, password))}  >Sign In</button>
+                    </div>
+                </form>
+            </div>
+
+        )
+}
+
