3d4050223ec63077a530856587dcae39e8e32304
Oleh Mandra <mandra9012003@gmail.com>
2022-07-10 20:51:28 +0300

    (Controllers) Created "OrderController"

---
+++ Web API/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using BLL.Interfaces;
+using BLL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Web_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        private readonly IOrderService _orderService;
+        public OrderController(IConfiguration configuration, IOrderService orderService)
+        {
+            _orderService = orderService;
+            _configuration = configuration;
+        }
+
+
+
+    }
+}
