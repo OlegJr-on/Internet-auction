484b4fc8db657423b9fb8585cea98a2935eec7ec
Oleh Mandra <mandra9012003@gmail.com>
2022-07-10 19:17:30 +0300

    (Controllers) Created "LotController"

---
+++ Web API/Controllers/LotController.cs
@@ -0,0 +1,31 @@
+﻿using BLL.Interfaces;
+using BLL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LotController : ControllerBase
+    {
+        private readonly ILotService _lotService;
+        private readonly IConfiguration _configuration;
+
+        public LotController(IConfiguration configuration, ILotService lotService)
+        {
+            _lotService = lotService;
+            _configuration = configuration;
+        }
+
+
+    }
+}
