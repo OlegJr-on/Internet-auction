9a2d15a9be5b9d51004c63de23679c02621eb886
Oleh Mandra <mandra9012003@gmail.com>
2022-07-10 23:39:16 +0300

    (pages) Added "Home.js" which represent "Home" section in site

---
+++ react-ui/src/pages/Home.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+
+
+class Home extends Component{
+    render(){
+        return (
+            <div className='home-page'>
+                <h2>Home</h2>
+                <h4>What Is Auto Auctions?</h4>
+                <p>Since 2016 Copart Germany has been remarketing salvage vehicles of fleet managers and insurance companies through online car auctions for national and international dealers, dismantlers and body shops.</p>
+                <img src='https://taurus-group.com.ua/images/blog/copart-image-usa.webp'/>
+                <p>Auctions at Copart Germany start at 10 a.m. (Europe/Berlin time) every Tuesday and Thursday, </p>
+                <p>and you can join them in real time from your computer or mobile device. </p>
+                Find, bid on and win salvage vehicles including ATVs, Automobiles, Classics, Motorcycles, Trucks and more by registering for Copart Membership. Once you’ve registered, you’ll be able to access our inventory, Watchlists, and Member Dashboard.
+                <p>As a global online car auction leader with hundreds of used cars for sale, Copart Germany offers full-service packages for every customer including pick up and vehicle transportation. In addition, Copart Germany buys salvage vehicles! Get a free quote to sell your used or damaged car now.</p>
+                <img src='https://cs.copart.com/v1/AUTH_svc.pdoc00001/PIX496/437dd1bd-a168-4ab1-b35d-8ccf4f64a3c4.JPG'/>
+            </div>
+        )
+    }
+}
+
+export {Home}
