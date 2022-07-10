e2aafc03cb5b7de12fda8e00861f40f7ee154cc5
Oleh Mandra <mandra9012003@gmail.com>
2022-07-10 16:06:31 +0300

    (Controllers) Create folder "Controllers" and added "UserController"

---
+++ Web API/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using BLL.Interfaces;
+using BLL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Web_API.Models;
+using System.Security.Claims;
+using DAL.Entities;
+
+namespace Web_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        private readonly IConfiguration _configuration;
+        public UserController(IConfiguration configuration, IUserService userService)
+        {
+            _userService = userService;
+            _configuration = configuration;
+        }
+
+    }
+}
